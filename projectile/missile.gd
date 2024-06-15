5328bab5c2c0719c563916cdd56f8d3f416de5ea
hugo <caroubalos.hugo@gmail.com>
2024-06-15 16:31:38 +0200

    Add simple synchronization for missile position without prediction

--- projectile/missile.gd
+++ projectile/missile.gd
@@ -3,6 +3,7 @@ extends RigidBody2D
 const MISSILE_SPEED = 400
 @export var explosion : PackedScene
 var projectile_id: int = -1
+@export var server_position: Vector2
 
 signal destroyed(_projectile_id:int)
 
@@ -11,6 +12,12 @@ func _ready():
 	center_of_mass = center_of_mass.rotated(rotation)
 	apply_impulse(MISSILE_SPEED * Vector2.RIGHT.rotated(rotation))
 
+func _process(_delta):
+	if Network.is_server():
+		server_position = position
+	else:
+		position = server_position
+	
 func destroy():
 	var explosion_instance = explosion.instantiate()
 	if explosion_instance:
